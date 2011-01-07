344c26dd2fa0c2199f3a76c558c2d17897b15430
Toliver Jue <teejae@gmail.com>
2011-01-08 02:49:52 +0900

    gofmt

--- otp/otp.go
+++ otp/otp.go
@@ -19,8 +19,8 @@ type Generator interface {
 type HOTPCounter uint64
 type HOTPGenerator struct {
 	counter HOTPCounter
-	digits int
-	hasher hash.Hash
+	digits  int
+	hasher  hash.Hash
 }
 
 func NewHOTPGenerator(secretKey []byte, initCounter HOTPCounter, digits int) *HOTPGenerator {
@@ -31,22 +31,22 @@ func (g *HOTPGenerator) Generate() string {
 	hasher := g.hasher
 	hasher.Reset()
 	defer hasher.Reset()
-	
+
 	binary.Write(hasher, binary.BigEndian, g.counter)
 	hashValue := hasher.Sum()
 	truncatedValue := dynamicTruncate(hashValue)
 	otpValue := truncatedValue % uint32(pow(10, int64(g.digits)))
-	
+
 	// increment counter
 	g.counter++
-	
+
 	// want a value with a specific number of digits
-	return fmt.Sprintf("%0" + strconv.Itoa(g.digits) + "d", otpValue)
+	return fmt.Sprintf("%0"+strconv.Itoa(g.digits)+"d", otpValue)
 }
 
 const (
-	DYNAMIC_TRUNCATE_OFFSET_BIT_MASK byte = 0x0F
-	DYNAMIC_TRUNCATE_31_BIT_MASK uint32 = 0x7FFFFFFF
+	DYNAMIC_TRUNCATE_OFFSET_BIT_MASK byte   = 0x0F
+	DYNAMIC_TRUNCATE_31_BIT_MASK     uint32 = 0x7FFFFFFF
 )
 
 // HOTP's dynamic truncate.
@@ -54,12 +54,12 @@ func dynamicTruncate(hashValue []byte) uint32 {
 	if len(hashValue) < 20 {
 		panic("the starting hash is too short!")
 	}
-	
+
 	offsetBits := hashValue[19] & DYNAMIC_TRUNCATE_OFFSET_BIT_MASK
 	// 0 <= offset <= 15
 	offset := uint8(offsetBits)
 
-	bits := binary.BigEndian.Uint32(hashValue[offset:offset + 4])
+	bits := binary.BigEndian.Uint32(hashValue[offset : offset+4])
 	truncated := bits & DYNAMIC_TRUNCATE_31_BIT_MASK
 	return truncated
 }
