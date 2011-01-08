f34d0922f003537c205292187fc6417e6bb9bfe1
Toliver Jue <teejae@gmail.com>
2011-01-09 08:36:34 +0900

    counter refactor

--- otp/otp.go
+++ otp/otp.go
@@ -14,31 +14,43 @@ type Generator interface {
 	Generate() string
 }
 
-// HMAC OTP implementation.
-// See: http://www.ietf.org/rfc/rfc4226.txt
-type HOTPCounter uint64
-type HOTPGenerator struct {
-	counter HOTPCounter
+type hotpCounter interface {
+	next() uint64
+}
+
+type hotpGenerator struct {
+	counter hotpCounter
 	digits  int
 	hasher  hash.Hash
 }
 
-func NewHOTPGenerator(secretKey []byte, initCounter HOTPCounter, digits int) Generator {
-	return &HOTPGenerator{counter: initCounter, digits: digits, hasher: hmac.NewSHA1(secretKey)}
+type hotpCounterImpl struct {
+	nextCounter uint64
 }
 
-func (g *HOTPGenerator) Generate() string {
+func (c *hotpCounterImpl) next() uint64 {
+	nextCounter := c.nextCounter
+	c.nextCounter++
+	return nextCounter
+}
+
+// HMAC OTP implementation.
+// See: http://www.ietf.org/rfc/rfc4226.txt
+func NewHOTPGenerator(secretKey []byte, initCounter uint64, digits int) Generator {
+	return &hotpGenerator{counter: &hotpCounterImpl{nextCounter: initCounter}, digits: digits, hasher: hmac.NewSHA1(secretKey)}
+}
+
+func (g *hotpGenerator) Generate() string {
 	hasher := g.hasher
 	hasher.Reset()
 	defer hasher.Reset()
 
-	binary.Write(hasher, binary.BigEndian, g.counter)
+	counter := g.counter.next()
+
+	binary.Write(hasher, binary.BigEndian, counter)
 	hashValue := hasher.Sum()
 	truncatedValue := dynamicTruncate(hashValue)
 	otpValue := truncatedValue % uint32(pow(10, int64(g.digits)))
-
-	// increment counter
-	g.counter++
 
 	// want a value with a specific number of digits
 	return fmt.Sprintf("%0"+strconv.Itoa(g.digits)+"d", otpValue)
